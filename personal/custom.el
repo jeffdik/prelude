8a79cb0c488c473c1edacb1d23375c9bc0ca854f
Jeff Dik <jdik@lonocloud.com>
2012-06-15 22:10:41 +0000

    added deft, smex configuration

--- personal/custom.el
+++ personal/custom.el
@@ -1,18 +1,22 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(bookmark-save-flag 1)
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(bookmark-save-flag 1 t)
+ '(deft-extension "org")
+ '(deft-text-mode (quote org-mode))
+ '(deft-use-filename-as-title t)
  '(etags-table-search-up-depth 20)
  '(mumamo-chunk-coloring 5 t)
  '(nxml-slash-auto-complete-flag t)
  '(org-agenda-files nil)
  '(org-hide-leading-stars t)
- '(org-odd-levels-only t))
+ '(org-odd-levels-only t)
+ '(whitespace-style (quote (face tabs spaces trailing lines space-before-tab newline indentation empty space-after-tab tab-mark newline-mark))))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(org-hide ((((background dark)) (:foreground "gray24")))))
---
+++ personal/jad.el
@@ -0,0 +1,8 @@
+(prelude-turn-off-whitespace)
+
+;; installed packages
+;; smex
+;; deft
+
+(global-set-key [f7] 'deft)
+(setq deft-directory (concat (getenv "HOME") "/org"))
